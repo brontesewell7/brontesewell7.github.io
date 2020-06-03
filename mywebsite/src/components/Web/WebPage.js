bce7972055e759a2ce47fe629a687c3dc210bff8
Brontesewell <brontsewell7@gmail.com>
2020-06-03 12:19:37 -0700

    text over home  image

--- mywebsite/src/components/Web/WebPage.js
+++ mywebsite/src/components/Web/WebPage.js
@@ -15,10 +15,12 @@ class WebPage extends Component {
         return (
             <div >
                 <WebNav/>
-<img id="web-top-image" src="https://images.unsplash.com/photo-1527691894785-52e19fab8bbc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1836&q=80"/>
-
+                <img id="web-top-image" src="https://images.unsplash.com/photo-1527691894785-52e19fab8bbc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1836&q=80"/>
+                <div id="quote-over-images"><h3 className="quote-home-image">Full Stack Web Developer and Designer</h3>
+                <a href='#section1' id="button-image-home">Learn More</a></div>
+                
                 <ScrollableAnchor id={'section1'}>
-                <button type="button" class="btn btn-outline-dark" onClick={()=> this.props.history.push('/')}>Back to Welcome Page</button>
+                <button style={{marginLeft: '42%'}} type="button" class="btn btn-outline-dark" onClick={()=> this.props.history.push('/')}>Back to Welcome Page</button>
                 </ScrollableAnchor>
             </div>
         );
