90ebaece7e8a5bb353a8434bfa334c5170841dc9
Brontesewell <brontsewell7@gmail.com>
2020-06-03 12:49:34 -0700

    about languages

--- mywebsite/src/components/LandingPage.js
+++ mywebsite/src/components/LandingPage.js
@@ -14,8 +14,8 @@ class LandingPage extends Component {
             <div className="App">
 
             <div class="wrapper">
-                <img src={horse}/>
-                <img src="https://images.unsplash.com/photo-1455894127589-22f75500213a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"/>
+                <img className="landing-page-img" src={horse}/>
+                <img className="landing-page-img-2" src="https://images.unsplash.com/photo-1455894127589-22f75500213a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"/>
                 <h1 id="landing-title">Bronte Sewell</h1>
              
                 <h2 id="equestrian-link"  onClick={()=> this.props.history.push('/equestrian')}>Equestrian</h2>
--- mywebsite/src/components/Web/WebPage.js
+++ mywebsite/src/components/Web/WebPage.js
@@ -18,10 +18,54 @@ class WebPage extends Component {
                 <img id="web-top-image" src="https://images.unsplash.com/photo-1527691894785-52e19fab8bbc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1836&q=80"/>
                 <div id="quote-over-images"><h3 className="quote-home-image">Full Stack Web Developer and Designer</h3>
                 <a href='#section1' id="button-image-home">Learn More</a></div>
+
+<br></br>
+<br></br>
+<br></br>
                 
                 <ScrollableAnchor id={'section1'}>
-                <button style={{marginLeft: '42%'}} type="button" class="btn btn-outline-dark" onClick={()=> this.props.history.push('/')}>Back to Welcome Page</button>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg fancy">
+                        <h3>Languages</h3>
+                        <ul>
+                            <li>Javascript</li>
+                            <li>Ruby</li>
+                            <li>Pyhton</li>
+                            <li>PHP</li>
+                            <li>HTML/CSS</li>
+                        </ul>
+                        <h3>Frameworks</h3>
+                        <ul>
+                            <li>Wordpress</li>
+                            <li>React</li>
+                            <li>Ruby on Rails</li>
+                            <li>React Native</li>
+                            <li>Redux</li>
+                            <li>NodeJS/Express</li>
+                            <li>GraphQL/Apollo</li>
+                            <li>Typescript</li>
+                            <li>SQL</li>
+                            <li>MongoDB</li>
+                            <li>Active Record</li>
+                            <li>Bootstrap/Semantic UI</li>
+                            <li>Adobe Photoshop</li>
+                            <li>Algorithms and Data Structures</li>
+                            <li>API's</li>
+                            <li>And More...</li>
+                        </ul>
+                        </div>
+                        <div class="col-lg">
+                        <h2 id="about-title">About</h2>
+                        </div>
+                    </div>
+                </div>
                 </ScrollableAnchor>
+
+
+
+
+                <button style={{marginLeft: '42%'}} type="button" class="btn btn-outline-dark" onClick={()=> this.props.history.push('/')}>Back to Welcome Page</button>
             </div>
         );
     }
