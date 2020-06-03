a6eb5548d9f77870bfc5171ec2a6f0f0fc3cfe4f
Brontesewell <brontsewell7@gmail.com>
2020-06-03 09:54:20 -0700

    landing page

--- mywebsite/src/App.js
+++ mywebsite/src/App.js
@@ -6,13 +6,15 @@ import horse from './horse.png'
 function App() {
   return (
     <div className="App">
-        {/* <a href='/equestrian'>Equestrian</a>
-
-        <a href='/web-developement'>Web Development and Design</a> */}
 
         <div class="wrapper">
           <img src={horse}/>
           <img src="https://images.unsplash.com/photo-1455894127589-22f75500213a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"/>
+          <h1 id="landing-title">Bronte Sewell</h1>
+         
+        <a id="equestrian-link" href='/equestrian'>Equestrian</a>
+
+        <a id="web-dev-link" href='/web-developement'>Web Development and Design</a>
         </div>
     </div>
   );
